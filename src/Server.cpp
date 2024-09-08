b7cb9f34ebfac39f81072bedf4307dad3b8fcde0
IanWearsHat <ianbbdai@gmail.com>
2024-09-07 17:56:45 -0700

    Fix for server

--- src/Server.cpp
+++ src/Server.cpp
@@ -65,15 +65,17 @@ int main(int argc, char **argv) {
   }
   
   // Block and wait for client to connect to server
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
   
-  std::cout << "Waiting for a client to connect...\n";
-  
-  int connfd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
   while (true) {
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    
+    std::cout << "Waiting for a client to connect...\n";
+    
+    int connfd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
     processClient(connfd);
+    close(connfd);
   }
   
   close(server_fd);
