aca88b83edaf5dcd48b82c3ff3bfb5988b1c2467
IanWearsHat <ianbbdai@gmail.com>
2024-09-08 22:00:21 -0700

    Server can handle multiple requests from the same client

--- src/Client.cpp
+++ src/Client.cpp
@@ -7,6 +7,86 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <assert.h>
+
+// Repeatedly reads from fd into buf until n bytes are reached
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+// Repeatedly writes from buf into fd until n bytes are written
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+const size_t k_max_msg = 4096;
+int32_t makeRequest(int fd, const char *text) {
+    uint32_t len = (uint32_t) strlen(text);
+
+    char wbuf[4 + len];
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], text, len);
+    int32_t err = write_all(fd, wbuf, 4 + len);
+    if (err) {
+        if (errno == 0) {
+            std::cout << "EOF" << std::endl;
+        } else {
+            std::cout << "write error" << std::endl;
+        }
+        return err;
+    }
+
+    // read response
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    err = read_full(fd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            std::cout << "EOF" << std::endl;
+        } else {
+            std::cout << "read() error" << std::endl;
+        }
+        return err;
+    }
+
+    // Get length of message from header
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        std::cout << "too long" << std::endl;
+        return -1;
+    }
+
+    // Read the number of bytes equal to length of message taken from header
+    err = read_full(fd, &rbuf[4], len);
+    if (err) {
+        std::cout << "read() error" << std::endl;
+        return err;
+    }
+
+    // Add end of line to buffer so printf can print
+    rbuf[4 + len] = '\0';
+    printf("server says: %s\n", &rbuf[4]);
+
+    return 0;
+}
 
 int main(int argc, char **argv) {
     // Flush after every std::cout / std::cerr
@@ -21,26 +101,34 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    for (int i = 0; i < 2; i++) {
-        struct sockaddr_in addr = {};
-        addr.sin_family = AF_INET;
-        addr.sin_port = ntohs(6379);
-        addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
-        int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
-        // if (rv) {
-        //     std::cout << "Error creating socket";
-        //     return 1;
-        // }
+    
+    
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(6379);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    // if (rv) {
+    //     std::cout << "Error creating socket";
+    //     return 1;
+    // }
 
-        char msg[] = "hello";
-        write(fd, msg, strlen(msg));
-
-        char rbuf[64] = {};
-        ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-        if (n < 0) {
-            std::cout << "Erro r reading response";
-        }
-        printf("server says: %s\n", rbuf);
+    // char msg[] = "*1\r\n$4\r\nPING\r\n";
+    int32_t err;
+    err = makeRequest(fd, "tory lanez");
+    if (err) {
+        close(fd);
+        return -1;
+    }
+    err = makeRequest(fd, "canada");
+    if (err) {
+        close(fd);
+        return -1;
+    }
+    err = makeRequest(fd, "just joshin");
+    if (err) {
+        close(fd);
+        return -1;
     }
     
     close(fd);
--- src/Server.cpp
+++ src/Server.cpp
@@ -7,87 +7,146 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <assert.h>
 
-void processClient(int connfd) {
-  if (connfd >= 0) std::cout << "Client connected\n";
-
-  // Read into buffer
-  char rbuf[64] = {};
- for (int i = 0; i < 2; i++) {
-    memset(rbuf, 0, sizeof(rbuf)); // Clear the buffer
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-      std::cout << "read() error";
-      return;
+// Repeatedly reads from fd into buf until n bytes are reached
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
     }
-
-    // rbuf[n] = '\0'; // Null-terminate the received data
-
-    printf("Client says: %s\n", rbuf);
-
-    // Write response
-    char wbuf[] = "+PONG\r\n";
-    write(connfd, wbuf, strlen(wbuf));
-
-    
-  }
-  
+    return 0;
 }
 
-int main(int argc, char **argv) {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+// Repeatedly writes from buf into fd until n bytes are written
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t) rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
-  
-  // Create socket - AF_INET for IPV4, SOCK_STREAM for TCP
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
-  }
-  
-  // Configure socket
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-  
-  // Bind to address 6379
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
-    return 1;
-  }
-  
-  // Enable listening for connections
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    std::cerr << "listen failed\n";
-    return 1;
-  }
-  
-  // Block and wait for client to connect to server
-  
-  int connfd;
+const size_t k_max_msg = 4096;
+int32_t processClient(int connfd) {
+    // Read 4 byte header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            std::cout << "EOF" << std::endl;
+        } else {
+            std::cout << "read() error" << std::endl;
+        }
+        return err;
+    }
 
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  
-  std::cout << "Waiting for a client to connect...\n";
-  
-  connfd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  processClient(connfd);
-  close(connfd);
+    // Get length of message from header
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        std::cout << "too long" << std::endl;
+        return -1;
+    }
 
-  
-  
-  close(server_fd);
+    // Read the number of bytes equal to length of message taken from header
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        std::cout << "read() error" << std::endl;
+        return err;
+    }
 
-  return 0;
+    // Add end of line to buffer so printf can print
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t) strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+
+    return 0;
+}
+
+    int main(int argc, char **argv) {
+    // Flush after every std::cout / std::cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+
+    // Create socket - AF_INET for IPV4, SOCK_STREAM for TCP
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        std::cerr << "Failed to create server socket\n";
+        return 1;
+    }
+
+    // Configure socket
+    int reuse = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        std::cerr << "setsockopt failed\n";
+        return 1;
+    }
+
+    // Bind to address 6379
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(6379);
+
+    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+        std::cerr << "Failed to bind to port 6379\n";
+        return 1;
+    }
+
+    // Enable listening for connections
+    int connection_backlog = 5;
+    if (listen(server_fd, connection_backlog) != 0) {
+        std::cerr << "listen failed\n";
+        return 1;
+    }
+
+    // Block and wait for client to connect to server
+
+    while (true) {
+        int connfd;
+
+        struct sockaddr_in client_addr;
+        int client_addr_len = sizeof(client_addr);
+
+        std::cout << "Waiting for a client to connect...\n";
+
+        connfd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+        if (connfd < 0) {
+            return 1;   // error
+        }
+        if (connfd >= 0) std::cout << "Client connected\n";
+
+        while (true) {
+            int32_t err = processClient(connfd);
+            if (err) {
+                break;
+            }
+        }
+
+        close(connfd);
+    }
+
+    close(server_fd);
+
+    return 0;
 }
